f910302e325879a4fd8fc679531f9e617ae8688e
Kartik-1902 <kartikdhingani0711@gmail.com>
2025-04-15 19:59:34 +0530

    added env fil

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://172.20.10.5:8000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode (e.g., 'development', 'production')
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,  // Use value from .env
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
-})
+  };
+});
